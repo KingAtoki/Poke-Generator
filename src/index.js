734005e03ae51cbe49389a52466452eacb2eaaba
Chris Atoki <christopher.atoki@yahoo.com>
2021-03-21 23:52:51 -0400

    Add Redux logic

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import store from './redux/store';
 import App from './App';
 import './index.css';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#F93318', // red
+      light: '#FFF933', // yellow
+      black: '#000000'
+    },
+  },
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/pokemon.actions.js
@@ -0,0 +1,45 @@
+export const GET_POKEMON = 'GET_POKEMON'
+export const GET_POKEMON_SUCCESS = 'GET_POKEMON_SUCCESS'
+export const GET_POKEMON_FAIL = 'GET_POKEMON_FAIL'
+export const GET_RANDOM_POKEMON = 'GET_RANDOM_POKEMON'
+export const GET_RANDOM_POKEMON_SUCCESS = 'GET_RANDOM_POKEMON_SUCCESS'
+export const GET_RANDOM_POKEMON_FAIL = 'GET_RANDOM_POKEMON_FAIL'
+
+
+export const addDefaultPokemon = () => async dispatch => {
+  try {
+    dispatch({ type: GET_POKEMON })
+    const pokemon = await Promise.all([
+      fetch('https://pokeapi.co/api/v2/pokemon/7/'),
+      fetch('https://pokeapi.co/api/v2/pokemon/4/'),
+      fetch('https://pokeapi.co/api/v2/pokemon/1/'),
+    ])
+    const pokeData = await Promise.all(pokemon.map(async p => await p.json()))
+    dispatch({ type: GET_POKEMON_SUCCESS, payload: { pokemon: pokeData } })
+  } catch (err) {
+    dispatch({ type: GET_POKEMON_FAIL, payload: { error: err.message } })
+    throw new Error(err)
+  }
+}
+
+const randomPokemonNumber = () => {
+  const randomize = () => Math.round(Math.random() * 100) // random num 1 - 100
+  const defaults = [1, 4, 7] // default pokemon
+  let num = randomize() // get random num
+  while (defaults.includes(num)) { // make sure num is not one of the defaults
+    num = randomize() // if it is, pull another number
+  }
+  return num
+}
+export const addRandomPokemon = () => async dispatch => {
+  try {
+    dispatch({ type: GET_RANDOM_POKEMON })
+    const randomPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonNumber()}/`)
+    const randomPokemonData = await randomPokemon.json()
+    dispatch({ type: GET_RANDOM_POKEMON_SUCCESS, payload: { randomPokemon: randomPokemonData } })
+
+  } catch (err) {
+    dispatch({ type: GET_RANDOM_POKEMON_FAIL, payload: { error: err.message } })
+    throw new Error(err)
+  }
+}
